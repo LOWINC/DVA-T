029415e4ac935ee0b0ce3f771f89e013dc55fcf7
sunweiqi <921939025@qq.com>
2020-03-19 11:41:06 +0800

    加入store中的put和页面中的action

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export {createDispatch} from "./utils/createDispatch";
-export {createSave} from "./utils/createSave";
 export {callFun} from "./utils/call";
+export {createEffect} from "./utils/createDispatch";
+export {createSave} from "./utils/createSave";
--- src/utils/createDispatch.ts
+++ src/utils/createDispatch.ts
@@ -1,11 +1,35 @@
-export function createDispatch<Effects extends any>(namespace?: string) {
+import {put, putResolve} from "redux-saga/effects";
+
+function createPut<Effects extends any>(namespace: string) {
   return function<Type extends keyof Effects>(
     type: Type,
-    payload: Parameters<Effects[Type]>[0]
+    payload?: Parameters<Effects[Type]>[0],
+    resolve?: false
+  ) {
+    const putEffect = resolve ? putResolve : put;
+
+    return putEffect({
+      type: `${namespace}/${type}`,
+      payload: ((payload || {}) as any).payload || {}
+    });
+  };
+}
+
+function createAction<Effects extends any>(namespace: string) {
+  return function<Type extends keyof Effects>(
+    type: Type,
+    payload?: Parameters<Effects[Type]>[0]
   ) {
     return {
-      type: namespace ? `${namespace}/${type}` : type,
-      payload: ((payload || {}) as any).payload
+      type: `${namespace}/${type}`,
+      payload: ((payload || {}) as any).payload || {}
     };
+  };
+}
+
+export function createEffect<T>(namespace: string) {
+  return {
+    put: createPut<T>(namespace),
+    action: createAction<T>(namespace)
   };
 }
