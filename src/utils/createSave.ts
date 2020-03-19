c3fa8dc6c120860525bd8137a88b3550e18243fa
sunweiqi <921939025@qq.com>
2020-03-19 11:40:35 +0800

    更新reducer

--- src/utils/createSave.ts
+++ src/utils/createSave.ts
@@ -1,10 +1,12 @@
-export function createSave<S>() {
+import {put} from "redux-saga/effects";
+
+export function createSave<S>(namespace) {
   return function<K extends keyof S>(key: K, value: S[K]) {
-    return {
-      type: "save",
+    return put({
+      type: `${namespace}/save`,
       payload: {
         [key]: value
       }
-    };
+    });
   };
 }
