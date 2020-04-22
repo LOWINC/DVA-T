049639042625c444767944475d64701aea12dbb6
sunweiqi <921939025@qq.com>
2020-04-22 18:33:57 +0800

    bugfix:put.resolve

--- src/utils/createEffect.ts
+++ src/utils/createEffect.ts
@@ -1,28 +1,28 @@
-import {put, putResolve} from "redux-saga/effects";
+import {put} from "redux-saga/effects";
 
 function createPut<Effects extends any>(namespace: string) {
-  return function<Type extends keyof Effects>(
+  return function <Type extends keyof Effects>(
     type: Type,
     payload: Parameters<Effects[Type]>[0],
-    resolve?: false
+    resolve = false
   ) {
-    const putEffect = resolve ? putResolve : put;
+    const putEffect = resolve ? (put as any).resolve : put;
 
     return putEffect({
       type: `${namespace}/${type}`,
-      payload: ((payload || {}) as any).payload || {}
+      payload: ((payload || {}) as any).payload || {},
     });
   };
 }
 
 function createAction<Effects extends any>(namespace: string) {
-  return function<Type extends keyof Effects>(
+  return function <Type extends keyof Effects>(
     type: Type,
     payload?: Parameters<Effects[Type]>[0]
   ) {
     return {
       type: `${namespace}/${type}`,
-      payload: ((payload || {}) as any).payload || {}
+      payload: ((payload || {}) as any).payload || {},
     };
   };
 }
@@ -30,6 +30,6 @@ function createAction<Effects extends any>(namespace: string) {
 export function createEffect<T>(namespace: string) {
   return {
     put: createPut<T>(namespace),
-    action: createAction<T>(namespace)
+    action: createAction<T>(namespace),
   };
 }
